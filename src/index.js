cfebcfb784329cdc3944747f91fbf5bd8a96c65b
Romanas Rodevičius <romanas.rodevicius@seb.se>
2017-08-06 23:48:14 +0300

    Adding basic error handling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 'use strict'
 
 console.log('----- Agora product API has started -----');
+
+process.on('uncoughtException', (err) => {
+  console.error('Unhandled Exception', err)
+})
+
+process.on('uncoughtRejection', (err, promise) => {
+  console.error('Unhandled Rejection', err)
+})
